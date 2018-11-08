fb04a87476c76c1d369910cc019eaf41c4361ca6
Ken Latimer <latimk@uw.edu>
2018-11-07 23:00:17 -0800

    widgets no longer class variables

--- gui/robot_data_visualizer.py
+++ gui/robot_data_visualizer.py
@@ -1,35 +1,33 @@
 """
 This file is the main viewing window of the application.
 """
-import tkinter as tk
-
-from numpy import arange, sin, pi
-
 import matplotlib
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib.backend_bases import key_press_handler
+from numpy import arange, sin, pi
+import tkinter as tk
 
 
 class RobotDataVisualizer(tk.Frame):
-
 
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.master.title("Robot Data Visualizer")
+        self.canvas  = None
         self.create_widgets()
 
     def create_widgets(self):
+        label = tk.Label(self.master, text="This is a label")
+        label.pack()
 
-        self.label = tk.Label(self.master, text="This is a label")
-        self.label.pack()
+        greet_button = tk.Button(self.master, text="Greet", command=self.greet)
+        greet_button.pack()
 
-        self.greet_button = tk.Button(self.master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = tk.Button(self.master, text="Close", command=self.master.destroy)
+        close_button = tk.Button(self.master, text="Close", command=self.master.destroy)
+        close_button.pack()
 
         f = Figure(figsize=(5, 4), dpi=100)
         a = f.add_subplot(111)
@@ -39,11 +37,11 @@ class RobotDataVisualizer(tk.Frame):
         a.plot(t, s)
 
         # a tk.DrawingArea
-        self.canvas = FigureCanvasTkAgg(f, master=root)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.canvas.mpl_connect('key_press_event', self.on_key_event)
-
+        canvas = FigureCanvasTkAgg(f, master=root)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.mpl_connect('key_press_event', self.on_key_event)
+        self.canvas = canvas
 
     def on_key_event(self, event):
 
@@ -54,7 +52,6 @@ class RobotDataVisualizer(tk.Frame):
     def greet(self):
 
         print("Greetings!")
-
 
 
 if __name__ == '__main__':
