5322c5c9d9a56715539abf35a808e6f30be44986
Ken Latimer <latimk@uw.edu>
2018-11-11 16:11:20 -0800

    basic layout

--- gui/robot_data_visualizer.py
+++ gui/robot_data_visualizer.py
@@ -6,29 +6,20 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from matplotlib.backend_bases import key_press_handler
+
 from numpy import arange, sin, pi
+
 import tkinter as tk
 
 
-class RobotDataVisualizer(tk.Frame):
+class VisualizerFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, bg="red")
+        self.parent = parent
+        self.canvas = None
+        self.widgets()
 
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.master.title("Robot Data Visualizer")
-        self.canvas  = None
-        self.create_widgets()
-
-    def create_widgets(self):
-        label = tk.Label(self.master, text="This is a label")
-        label.pack()
-
-        greet_button = tk.Button(self.master, text="Greet", command=self.greet)
-        greet_button.pack()
-
-        close_button = tk.Button(self.master, text="Close", command=self.master.destroy)
-        close_button.pack()
-
+    def widgets(self):
         f = Figure(figsize=(5, 4), dpi=100)
         a = f.add_subplot(111)
         t = arange(0.0, 3.0, 0.01)
@@ -37,21 +28,72 @@ class RobotDataVisualizer(tk.Frame):
         a.plot(t, s)
 
         # a tk.DrawingArea
-        canvas = FigureCanvasTkAgg(f, master=root)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        canvas.mpl_connect('key_press_event', self.on_key_event)
-        self.canvas = canvas
+        self.canvas = FigureCanvasTkAgg(f, master=self.master)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # self.canvas.get_tk_widget().grid(row=0, column=0)
+        self.canvas.mpl_connect('key_press_event', self.on_key_event)
+
+        # Label for visualizer
+        self.label = tk.Label(self, text="Viewer")
+        self.label.pack()
 
     def on_key_event(self, event):
         print('you pressed %s' % event.key)
         key_press_handler(event, self.canvas)
 
-    def greet(self):
-        print("Greetings!")
+
+class ToolbarFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.widgets()
+
+    def widgets(self):
+        insert_button = tk.Button(self, text="Insert Image", command=self.do_nothing)
+        insert_button.pack(side=tk.LEFT, padx=2, pady=2)
+        print_button = tk.Button(self, text="Print", command=self.do_nothing)
+        print_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+    def do_nothing(self):
+        print("Ok, ok, I won't ...")
+
+class SlamControl(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, width=400)
+        self.parent = parent
+        self.widgets()
+
+    def widgets(self):
+        label = tk.Label(self, text="SLAM Control Frame", bg="red", fg="white")
+        label.pack(fill=tk.X)
+
+
+# Main window for the application
+class MainWindow(tk.Tk):
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.title("Robot Data Visualizer")
+        self.mainWidgets()
+
+    def mainWidgets(self):
+        self.toolbar = ToolbarFrame(self)
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        # Status bar
+        self.status = tk.Label(self, text="Preparing to do nothing", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.status.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # Controls for running the SLAM algorithm
+        self.control = SlamControl(self)
+        self.control.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # Main viewing window
+        self.window = VisualizerFrame(self)
+        self.window.pack(side=tk.LEFT)
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    gui = RobotDataVisualizer(master=root)
-    root.mainloop()
+    app = MainWindow(None)
+    app.mainloop()
