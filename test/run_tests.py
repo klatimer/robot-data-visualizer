aca5efa812a3d755e11412c4fc5c54a115c55aae
Ken Latimer <latimk@uw.edu>
2018-12-10 11:03:58 -0800

    updated to throw exit code 1 for failed tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 
 test_modules = ["test_sample", "test_data_loader"]
@@ -7,4 +8,5 @@ suite = unittest.TestSuite()
 for t in test_modules:
     suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
 
-unittest.TextTestRunner().run(suite)
+result = not unittest.TextTestRunner().run(suite).wasSuccessful()
+sys.exit(result) # Throw exit code 1 if tests failed
