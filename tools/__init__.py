5345c89af88e12ed0f6df370de6e62b07cbbfbc3
Ken Latimer <latimk@uw.edu>
2018-12-09 23:21:03 -0800

    bug fix in import order

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,6 +1,6 @@
+from .get_dates_umich import get_dates_umich
 from .static_map_base_layer import StaticMapBaseLayer
 from .staticmap_for_gps import map_for_gps
-from .get_dates_umich import get_dates_umich
 from .data_loader import DataLoader
 from .data_manager import DataManager
 from .download_tar import download_tar
