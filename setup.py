f850fcc6993dc53f561ca8e624813559b881a67e
Hao Wu <wh1210@uw.edu>
2018-12-10 23:35:41 -0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 """A setuptools based setup module"""
 from setuptools import setup, find_packages
 
+PACKAGES = find_packages()
+
 setup(
 	name = "robot-data-visualizer",
 	version = "0.1",
-	packages = find_packages(),
+    url='https://github.com/klatimer/robot-data-visualizer',
+    license='MIT',
+    packages=PACKAGES,
+    author='Ray Adler, Ken Latimer, Hao Wu, Robin Li',
+    author_email='wh1210@uw.edu, To Be Added'
 )
