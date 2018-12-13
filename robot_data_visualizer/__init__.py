de5945f2dfe50a81ec148bf3870af5172bdd8df1
Ken Latimer <latimk@uw.edu>
2018-12-12 21:59:45 -0800

    moved imports in init

--- robot_data_visualizer/__init__.py
+++ robot_data_visualizer/__init__.py
@@ -1,11 +1,6 @@
 __all__ = ['data_loader', 'data_manager', 'download_tar', 'get_dates_umich', 'map_for_gps', 'read_hokuyo', 'static_map_base_layer', 'staticmap_for_gps', 'tar_extract', 'hokuyo_plot', 'threshold_lidar_pts', 'epoch_to_date_time']
 
-from .static_map_base_layer import StaticMapBaseLayer
 from .get_dates_umich import get_dates_umich
-from .time_convert import epoch_to_date_time
-from .view_lidar import hokuyo_plot
-from .view_lidar import threshold_lidar_pts
-from .staticmap_for_gps import map_for_gps
 from .data_loader import DataLoader
 from .data_manager import DataManager
 from .download_tar import download_tar
@@ -14,3 +9,8 @@ from .tar_extract import tar_extract
 from .get_dates_umich import get_dates_umich
 from .view_lidar import hokuyo_plot
 from .view_lidar import threshold_lidar_pts
+from .time_convert import epoch_to_date_time
+from .view_lidar import hokuyo_plot
+from .view_lidar import threshold_lidar_pts
+from .static_map_base_layer import StaticMapBaseLayer
+from .staticmap_for_gps import map_for_gps
